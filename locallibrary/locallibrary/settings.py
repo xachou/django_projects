3d01499a15d4e5c63c785428d5437ab487bcc543
Xunan Zhou <andyzhou@umich.edu>
2019-01-15 03:46:52 +0000

    After Sekeleton

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')2e*^-$zxg9!tuq=5f-d5d)mvq28u841qu1m#942l1n6%^jnmt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
