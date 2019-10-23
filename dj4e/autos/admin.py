7b3245d7a52cbd5d8041c48f8d793a2d23cd38cd
Xunan Zhou <andyzhou@umich.edu>
2019-10-23 22:48:34 +0000

    save locallibrary and workon Cats CRUD

---
+++ dj4e/autos/admin.py
@@ -0,0 +1,9 @@
+# Register your models here.
+from django.contrib import admin
+
+from autos.models import Make, Auto
+
+# Register your models here.
+
+admin.site.register(Make)
+admin.site.register(Auto)
