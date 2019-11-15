90ee62163c07a0eddfde44be8ac828d3d609c950
Xunan Zhou <andyzhou@umich.edu>
2019-11-15 17:38:35 +0000

    ad#1

---
+++ adlist/ads/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from ads.models import Ad
+# Register your models here.
+admin.site.register(Ad)
---
+++ adlist/ads/views.py
@@ -0,0 +1,30 @@
+from ads.models import Ad
+
+from django.views import View
+from django.views import generic
+from django.shortcuts import render
+
+from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+
+class AdListView(OwnerListView):
+    model = Ad
+    template_name = "ads/ads_list.html"
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+    template_name = "ads/ads_detail.html"
+
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    fields = ['title', 'text', 'price']
+    template_name = "ads/ads_form.html"
+
+class AdUpdateView(OwnerUpdateView):
+    model = Ad
+    fields = ['title', 'text', 'price']
+    template_name = "ads/ads_form.html"
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
+    template_name = "ads/ads_delete.html"
+
